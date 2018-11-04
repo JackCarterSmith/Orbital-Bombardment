afa29f9b69d0990b1f1f6dfe30dbda5283d3b570
JackCarterSmith <arechristopher1@gmail.com>
2018-11-04 15:15:13 +0100

    New base, cleaned code

---
+++ src/main/java/jackcartersmith/orbsat/client/ClientProxy.java
@@ -0,0 +1,10 @@
+package jackcartersmith.orbsat.client;
+
+import jackcartersmith.orbsat.common.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(Side.CLIENT)
+public class ClientProxy extends CommonProxy{
+
+}
